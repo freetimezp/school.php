ffe5af8abdf15a4cfb6420be51e045757f88bfa4
freetimezp <kraftukrnet@ukr.net>
2022-04-17 21:49:06 +0300

    48 staff/users search

--- private/controllers/Users.php
+++ private/controllers/Users.php
@@ -14,7 +14,17 @@ class Users extends Controller
         $crumbs[] = ['Dashboard', ''];
         $crumbs[] = ['Staff', 'users'];
 
-        $data = $user->query("SELECT * FROM users WHERE school_id = :school_id AND rank NOT IN ('student') ORDER BY id DESC", ['school_id' => $school_id]);
+
+        $query = "SELECT * FROM users WHERE school_id = :school_id AND rank NOT IN ('student') ORDER BY id DESC";
+        $arr['school_id'] = $school_id;
+
+        if(isset($_GET['find'])) {
+            $find = '%' . $_GET['find'] . '%';
+            $query = "SELECT * FROM users WHERE school_id = :school_id AND rank NOT IN ('student') AND (firstname LIKE :find OR lastname LIKE :find)  ORDER BY id DESC";
+            $arr['find'] = $find;
+        }
+
+        $data = $user->query($query, $arr);
 
         if(Auth::access('admin')) {
             $this->view('users', [
--- private/views/users.view.php
+++ private/views/users.view.php
@@ -10,12 +10,11 @@ $this->view('includes/nav');
             <form class="form-inline">
                 <div class="input-group">
                     <div class="input-group-prepend">
-                        <span class="input-group-text" id="basic-addon1"><i class="fa fa-search"></i>&nbsp;</span>
+                        <button class="input-group-text" id="basic-addon1"><i class="fa fa-search"></i>&nbsp;</button>
                     </div>
-                    <input type="text" class="form-control" placeholder="Search" aria-label="Search" aria-describedby="basic-addon1">
+                    <input value="<?=isset($_GET['find'])?$_GET['find']:'';?>" name="find" type="text" class="form-control" placeholder="Search" aria-label="Search" aria-describedby="basic-addon1">
                 </div>
             </form>
-
 
             <a href="<?=ROOT;?>/signup">
                 <button class="btn btn-sm btn-primary"><i class="fa fa-plus"></i>Add new</button>
